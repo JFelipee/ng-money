9814909ada50228081e837465f646430c0afc9a5
João Felipe <vilelajfelipe@gmail.com>
2024-05-09 23:25:55 -0300

    feat: Configura rotas no aplicativo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TransactionListComponent } from './transaction-list/transaction-list.component';
+import { AddTransactionComponent } from './add-transaction/add-transaction.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TransactionListComponent },
+  { path: 'add', component: AddTransactionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
